72d4e9489b72327bdf0a6a8a08fff8708c85c0a9
ilivanoff <ivanov.ilya.alex@gmail.com>
2016-02-17 23:26:43 +0300

    gitall

--- gitall.bat
+++ gitall.bat
@@ -4,6 +4,7 @@ rem git pull
 rem git submodule init
 
 git submodule update --init
+git submodule foreach git checkout master
 git submodule foreach git pull origin master
 
 rem Если передан какой-либо параметр, то не ожидаем
